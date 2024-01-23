40878bf1e7a093fa619b5d005ad179e82a382e59
priyadiwaker45 <157494593+priyadiwaker45@users.noreply.github.com>
2024-01-23 19:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#p2").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $(".btn2").click(function(){
+      $("#p3").slideDown("slow");
+    })
+  })
+
+  $(document).ready(function(){
+    $(".btn3").click(function(){
+      $(".t4").slideDown("slow")
+    })
+
+  })
